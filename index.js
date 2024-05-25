7d7c75ff23ca088b5f40305e1de55dc410629470
David Chen <chen.david.1016@gmail.com>
2024-05-25 16:55:12 -0500

    import package for create csv, added comments, removed console.logs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // EDIT THIS FILE TO COMPLETE ASSIGNMENT QUESTION 1
 const { chromium } = require("playwright");
+const { createObjectCsvWriter } = require("csv-writer");
 
+// Create articles array to store list of objects with key/values for title and url
 let articles = [];
 
 async function saveHackerNewsArticles() {
@@ -9,23 +11,28 @@ async function saveHackerNewsArticles() {
   const context = await browser.newContext();
   const page = await context.newPage();
 
-  // go to Hacker News
+  // go to Hacker News (this was already set to the correct URL based on provided project files)
   await page.goto("https://news.ycombinator.com");
 
-  const titleElements = await page.$$('.titleline');
+  // Find elements on page where class attribute is titleline. (I went to the website and inspected the element I needed)
+  const titleElements = await page.$$('.titleline'); // returns an array
 
+  // Look at the first 10 elements (another option would be narrow the results by looking elements whose rank is equal to 1 through 10, but that felt like more code)
   for (let i = 0; i < 10; i++){
 
     currentElement = titleElements[i];
 
     const title = await page.evaluate((element) => {
-      // Select all elements with class 'sitestr' within the element
+
+      // Select all elements with class 'sitestr' within the element (had to do this bc I kept getting the sitestr url appended to the title)
       const sitestrElements = element.querySelectorAll(".sitestr");
+
       // Exclude the text content of 'sitestr' elements from the parent element's textContent
       sitestrElements.forEach((sitestrElement) => {
         sitestrElement.remove();
       });
-      // Return the text content after trimming leading and trailing whitespace and remove the '()' 
+
+      // Return the title after trimming leading and trailing whitespace and remove the '()' 
       return element.textContent.trim().replace(/\s\(\)$/, ''); 
     }, currentElement);
   
@@ -40,10 +47,20 @@ async function saveHackerNewsArticles() {
     articles.push({title: title, url: url})
   }
   
+  // Implement csv file creator [npm i csv-writer]
+  const csvWriter = createObjectCsvWriter({
+    path: 'articles.csv',
+    header: [
+      {id: 'title', title: 'Title'},
+      {id: 'url', title: 'URL'}
+    ]
+  });
 
+  // Create the csv file
+  csvWriter.writeRecords(articles);
+  
+  // Close the browswer
   await browser.close();
-  console.log(articles)
-  console.log(articles.length)
 }
 
 (async () => {
