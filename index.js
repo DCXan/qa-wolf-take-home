46998cd2d0be18c244620afca1e8886f7279d9a1
David Chen <chen.david.1016@gmail.com>
2024-05-25 15:40:41 -0500

    small refactor to return title with out parentheses

--- index.js
+++ index.js
@@ -10,19 +10,22 @@ async function saveHackerNewsArticles() {
   // go to Hacker News
   await page.goto("https://news.ycombinator.com");
 
-  const element = await page.waitForSelector(".titleline");
+  const titleElement = await page.waitForSelector(".titleline");
 
-  const labelText = await page.evaluate((element) => {
+  const title = await page.evaluate((element) => {
     // Select all elements with class 'sitestr' within the element
     const sitestrElements = element.querySelectorAll(".sitestr");
     // Exclude the text content of 'sitestr' elements from the parent element's textContent
     sitestrElements.forEach((sitestrElement) => {
       sitestrElement.remove();
     });
-    return element.textContent.trim(); // Return the text content after trimming leading and trailing whitespace
-  }, element);
+    // Return the text content after trimming leading and trailing whitespace and remove the '()' 
+    return element.textContent.trim().replace(/\s\(\)$/, ''); 
+  }, titleElement);
 
-  console.log(labelText.replace(/\s\(\)$/, ''));
+  
+
+  console.log(title);
 
   await browser.close();
 }
